780b025304a93cd47ef380f191254a9012196c38
Jeffrey <Jeffrey.zksun@gmail.com>
2012-08-30 21:44:34 +0800

    Update php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Hello world</title>
+  </head>
+  <body>
+  <?php echo 'Hello World'; ?> 
+  <?php phpinfo(); ?>
+  </body>
+</html>
