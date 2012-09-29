2797728498e788d8223861f41d8a5446bd61c016
Jeffrey <Jeffrey.zksun@gmail.com>
2012-09-29 20:17:23 +0800

    update

--- WebSite/server.js
+++ WebSite/server.js
@@ -9,20 +9,21 @@ var app = express();
 
 // Configurations
 app.set('port', process.env.PORT || 3000); // Configure the listening port
-
 app.set('views', __dirname + '/views'); // The folder for the views, such as .html. The folder is <website>/views/
-app.engine('html', require('ejs').renderFile); // Set the render engine for .html file.
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public')); // Static resources.
+
+app.engine('html', require('ejs').renderFile);
 
 // routers
-app.get('/', function(req, res){
-  res.send(__dirname + ":" + __filename);
-   
+app.get('/', function(req, res, next){
+   res.render('../public/design.html');
 });
 
-app.get('/about', function (req, res)
+app.get('/login', function (req, res)
 {
-	console.log('about');
-    res.render('about.html');
+    res.render('login');
 });
 
 app.listen(app.get('port'));
